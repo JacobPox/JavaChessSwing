78abeef8bc9e5974d9e978a27ba5b355490664fc
Nwc5237 <38166403+Nwc5237@users.noreply.github.com>
2019-01-29 20:34:35 -0500

    Added Bishop.java
    for the bishop to make a legal move it has to move diagonally from its current position. To make sure that the new x and y-values follow this rule we check that abs(deltaX) == abs(deltaY).

---
+++ Bishop.java
@@ -0,0 +1,18 @@
+package piece;
+public class Bishop extends Piece
+{
+    public Bishop(String player, int x, int y)
+    {
+        //setting initial board position
+        super(player, "bishop", x, y);
+    }
+    
+    public boolean legalMove(int finX, int finY)
+    {
+        //changes in x and y
+        int deltaX = finX - this.curX;
+        int deltaY = finY - this.curX;
+        
+        //to be a diagonal line the change in x must be +/- the change in y
+        return (deltaX == deltaY || deltaX == -deltaY) && (deltaX != 0);
+    }
