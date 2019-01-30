c62c61e241decd377c8282e8f372d291c0199455
Nwc5237 <38166403+Nwc5237@users.noreply.github.com>
2019-01-29 20:12:49 -0500

    Added exclusive or
    Made it so that the rook only moves along y or x, but doesn't not move (because that's not a move)

--- Rook.java
+++ Rook.java
@@ -1,9 +1,6 @@
-
 package piece;
 public class Rook extends Piece
-{
-    //private final String player;
-    
+{   
     public Rook(String player, int x, int y)
     {
         //setting initial board position
@@ -13,7 +10,7 @@ public class Rook extends Piece
     @Override
     public boolean moveLegal(int finX, int finY)
     {
-        //either x or y must remain the same because rooks only move up or to the side
-        return this.curX == finX || this.curY == finY;
+        //x is unchanged xor y is unchanged
+        return (finX == this.curX) ^ (finY == this.curY);
     }
 }
