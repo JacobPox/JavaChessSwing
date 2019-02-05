8aeae022da00d35d3ab840898758fda61e92a20a
Nwc5237 <38166403+Nwc5237@users.noreply.github.com>
2019-02-05 13:23:00 -0500

    added moveLegal override
    checks if it moves one

--- King.java
+++ King.java
@@ -1,10 +1,21 @@
+import java.lang.*;
+
 public class King extends Piece
 {   
-    public King(String player, String piece, int x, int y)
+    public King(String player, int x, int y)
     {
-        //setting initial board position
-
-        super(player, piece, x, y);
+        super(player, "king", x, y);
+    }
+    
+    @Override
+    public boolean moveLegal(int x, int y)
+    {
+        int deltaX = x - this.curX;
+        int deltaY = y - this.curY;
+        if((Math.abs(deltaX) == 1 && deltaY == 0) || (Math.abs(deltaY) == 1 && deltaX == 0) || (Math.asb(deltaY) == 1 && Math.abs(deltaX == 1)))
+            return true;
+        else
+            return false;
     }
     
 }
