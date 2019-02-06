1d0afad54ccd4ef5bf0d57db9eaff0e787b9eecb
Nwc5237 <38166403+Nwc5237@users.noreply.github.com>
2019-02-06 15:32:47 -0500

    updated x to curX
    fixed from old super class variable name

--- Rook.java
+++ Rook.java
@@ -10,8 +10,8 @@ public class Rook extends Piece
     @Override
     public boolean moveLegal(int finX, int finY)
     {
-        int deltaX = finX - this.x;
-        int deltaY = finY - this.y;
+        int deltaX = finX - this.curX;
+        int deltaY = finY - this.curY;
         //x is unchanged xor y is unchanged
         if(!(deltaX == 0 && deltaY == 0))
             return (finX == this.curX) ^ (finY == this.curY);
