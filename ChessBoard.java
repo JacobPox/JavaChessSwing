97ff9a5f31d0edc2a877a907494b85242952424d
Nwc5237 <38166403+Nwc5237@users.noreply.github.com>
2019-02-06 15:50:09 -0500

    Added a divider to the leftmost space

--- ChessBoard.java
+++ ChessBoard.java
@@ -90,6 +90,7 @@ public class ChessBoard {
     public void presentBoard() {
       //Prints board
     for (int i = 0; i < 8; i++) {
+      System.out.print("| ");
         for (int j = 0; j < 8; j++) {
             System.out.print(board[i][j] + " | ");
         }
