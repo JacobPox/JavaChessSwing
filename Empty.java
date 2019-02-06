ce0c9e290ea36bb8f48efa7fe2f103c807a418e1
Jacob <41162160+JacobPox@users.noreply.github.com>
2019-02-06 16:24:24 -0500

    Added empty class
    Empty class represents a blank spot, it is not a piece.

---
+++ Empty.java
@@ -0,0 +1,11 @@
+public class Empty extends Piece {
+
+    public Empty(int x, int y)
+    {
+        /*
+        This class represents a space not occupied and is treated as nothing.
+         */
+
+        super("neutral", "", x, y);
+    }
+}
