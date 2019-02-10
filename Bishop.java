cd931bb97b6dd38f136b966a6a5e13aa645ea997
Nwc5237 <38166403+Nwc5237@users.noreply.github.com>
2019-02-10 00:34:20 -0500

    Added the instance of ChessBoard to constructor

--- Bishop.java
+++ Bishop.java
@@ -1,9 +1,11 @@
+package projectoverlord.ProjectOverlord;
+
 public class Bishop extends Piece
 {
-    public Bishop(String player, String piece, int x, int y)
+    public Bishop(ChessBoard board, String player, String piece, int x, int y)
     {
         //setting initial board position
-        super(player, piece, x, y);
+        super(board, player, piece, x, y);
     }
     
     @Override
@@ -11,7 +13,7 @@ public class Bishop extends Piece
     {
         //changes in x and y
         int deltaX = finX - this.curX;
-        int deltaY = finY - this.curX;
+        int deltaY = finY - this.curY;
         
         //to be a diagonal line the change in x must be +/- the change in y
         return (deltaX == deltaY || deltaX == -deltaY) && (deltaX != 0);
