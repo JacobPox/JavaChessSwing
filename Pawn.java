ceb794749c60fa1e8b088f209e27e1f1e06b1365
Jacob <41162160+JacobPox@users.noreply.github.com>
2019-02-12 11:20:08 -0500

    Fix pawn moving two positions
    There was a problem with determining whether a pawn on the black side could move two or not. The problem was because although that row is position 7 on the board, it really is index 1 in the array. Now both sides can move twice if they haven't moved yet.

--- Pawn.java
+++ Pawn.java
@@ -1,5 +1,3 @@
-package projectoverlord.ProjectOverlord;
-
 import java.lang.*;
 
 public class Pawn extends Piece
@@ -17,17 +15,19 @@ public class Pawn extends Piece
     public boolean moveLegal(int finX, int finY)
     {
         /* Check to see if the pawn is on the starting row. If it is on the starting row, it can move forward twice. It doesn't matter 
-        which starting row it is on (row 2 or 6) because it would move out of bounds anyways if it was on the enemies home row and tried
+        which starting row it is on (row 2 or 7, but the curY really is -1 to each because indexing starts at 0)
+        because it would move out of bounds anyways if it was on the enemies home row and tried
         to move twice. 
         */
         
-        if (!(this.curY == 2 || this.curY == 6))
+        if (!(this.curY == 1 || this.curY == 6))
         {
             hasMoved = true;
         }
         
         int deltaX = finX - this.curX;
         int deltaY = finY - this.curY;
+
         
         /* Cases
         1. Moved forward once
@@ -38,7 +38,7 @@ public class Pawn extends Piece
         return
         (
             (Math.abs(deltaY) == 1 && deltaX == 0 && board.isEmpty(finX, finY)) ||
-            (Math.abs(deltaY) == 2 && deltaX == 0 && hasMoved == false && board.isEmpty(finX, finY)) ||
+            (Math.abs(deltaY) == 2 && deltaX == 0 && !hasMoved && board.isEmpty(finX, finY)) ||
             (Math.abs(deltaY) == 1 && Math.abs(deltaX) == 1 && isTakeable(finX, finY) && !board.isEmpty(finX, finY))
         );
         
