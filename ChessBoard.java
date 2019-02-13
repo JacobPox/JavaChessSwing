b16aa7f00e73b6a655d72ffdcb09b5ea0b2123fa
Nwc5237 <38166403+Nwc5237@users.noreply.github.com>
2019-02-12 20:56:00 -0500

    Message telling user which team is white and black

--- ChessBoard.java
+++ ChessBoard.java
@@ -39,7 +39,7 @@ public class ChessBoard
     {
         board = new Piece[8][8];
         
-        System.out.println("New game beginning!");
+        System.out.println("New game beginning!\nWhite pieces are lowercase\nBlack pieces are uppercase");
         fillBoard();
         presentBoard();
     }
