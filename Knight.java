c46d0af6dac4433d87f5dd289f38ecd3709e28b1
Nwc5237 <38166403+Nwc5237@users.noreply.github.com>
2019-03-19 17:24:34 -0400

    Made the move legal method more compact

--- Knight.java
+++ Knight.java
@@ -12,24 +12,10 @@ public class Knight extends Piece
     public boolean moveLegal(int finX, int finY)
     {
         //eight legal moves
-        if((finX == this.curX+1) && (finY == this.curY + 2))
-            return true;
-        if((finX == this.curX+1) && (finY == this.curY - 2))
-            return true;
-        if((finX == this.curX-1) && (finY == this.curY + 2))
-            return true;
-        if((finX == this.curX-1) && (finY == this.curY - 2))
-            return true;
+        int deltaX = finX - curX;
+        int deltaY = finY - curY;
         
-        if((finY == this.curY+1) && (finX == this.curX + 2))
-            return true;
-        if((finY == this.curY+1) && (finX == this.curX - 2))
-            return true;
-        if((finY == this.curY-1) && (finX == this.curX + 2))
-            return true;
-        if((finY == this.curY-1) && (finX == this.curX - 2))
-            return true;
-        return false;
+        return (Math.abs(deltaX) == 2 && Math.abs(deltaY) == 1) || (Math.abs(deltaY) == 2 && Math.abs(deltaX) == 1);
     }
     
     //knights dont care if the path is clear
